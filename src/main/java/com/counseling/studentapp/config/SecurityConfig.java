51cc1299dc0ff086b3849905d4bc86a331756899
Prakharjstar <prakharjoshi960@gmail.com>
2025-08-25 23:30:38 +0530

    Update SecurityConfig.java

--- src/main/java/com/counseling/studentapp/config/SecurityConfig.java
+++ src/main/java/com/counseling/studentapp/config/SecurityConfig.java
@@ -43,8 +43,8 @@ private CustomSuccessHandler customSuccessHandler;
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/signup", "/register", "/css/**", "/images/**","/js/**", "/oauth2/**" ,"/login/oauth2/**").permitAll()
-                .requestMatchers("/admin/**").hasRole("ROLE_ADMIN")       // For role-based access
-                .requestMatchers("/student/**").hasRole("ROLE_STUDENT")
+                .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")       // For role-based access
+                .requestMatchers("/student/**").hasAuthority("ROLE_STUDENT")
                 .requestMatchers("/dashboard").authenticated()
                 .anyRequest().authenticated()
             )
